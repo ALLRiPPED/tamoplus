5b33047bb3ecaba276b39717d7d2f31d55bf0168
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-06-25 00:14:17 -0500

    Create temp-fan.sh

---
+++ scripts/temp-fan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ontemp=45
+temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+echo $temp #use BCM pin 14 for Ice Tower
+temp0=${temp%.*}
+echo $temp0
+
+if [ $temp0 -gt $ontemp ]
+then
+        echo greater than $ontemp fan on
+        raspi-gpio set 14 op dh
+else
+        echo less than or equal to  $ontemp fan off
+        raspi-gpio set 14 op dl
+fi
