f415ceb75510ccee96923f05a5f4aaf389a79083
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-07-05 23:27:22 -0500

    Update tamoplus.sh

--- files/tamoplus.sh
+++ files/tamoplus.sh
@@ -7,9 +7,9 @@ currentuser=$(whoami) # Check user
 
 #Auto Updater
 if grep 'auto_update_flag=1' "$USER_SETTINGS"; then
-	if dialog --stdout --title "What to do?" \
+	if dialog --stdout --title "Contiue Auto-Update?" \
 			--backtitle "Contiue Auto-Update?" \
-			--yesno "Yes: Update, No: Skip" 7 60; then
+			--yesno "Yes: Continue Auto-Update, No: Skip Auto-Update" 7 60; then
 		bash $INSTALL_DIR/scripts/updater.sh
 		exit 1
 	else
